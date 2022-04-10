86e5549170ae9ae2e10f14c74571d9ccdd65efbc
Decagon <decagon@Isiomas-MacBook-Air.local>
2022-04-10 19:52:29 +0100

    created grp b project

---
+++ src/main/java/com/groupb/week8todoapp/Week8TodoAppApplication.java
@@ -0,0 +1,13 @@
+package com.groupb.week8todoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week8TodoAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week8TodoAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/groupb/week8todoapp/Week8TodoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groupb.week8todoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week8TodoAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
