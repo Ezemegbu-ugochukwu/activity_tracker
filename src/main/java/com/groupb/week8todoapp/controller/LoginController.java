8078cdf0e49cf5a292df84d5eb24324b65e66205
Darlington Olelewe <darlington.olelewe@decagon.dev>
2022-04-12 19:29:15 +0100

    Login Successful and Forward Into Dashboard

--- src/main/java/com/groupb/week8todoapp/controller/LoginController.java
+++ src/main/java/com/groupb/week8todoapp/controller/LoginController.java
@@ -1,13 +1,21 @@
 package com.groupb.week8todoapp.controller;
 
+import com.groupb.week8todoapp.dto.LoginDto;
+import com.groupb.week8todoapp.model.User;
+import com.groupb.week8todoapp.services.UserServices;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/pages")
 public class LoginController {
+
+    @Autowired
+    private UserServices userServices;
 
 //    @GetMapping("/signin")
 //    public String signin(){
@@ -15,7 +23,29 @@ public class LoginController {
 //    }
 
     @GetMapping("/login")
-    public String login(){
+    public String login(Model model){
+        LoginDto loginDto = new LoginDto();
+        model.addAttribute("loginDto", loginDto);
         return "pages/login";
     }
+
+    @PostMapping("/login")
+    public String login(@ModelAttribute("loginDto") LoginDto loginDto, HttpSession session, Model model){
+
+        User user = userServices.login(loginDto);
+
+
+        if(user == null){
+            String message = "Email or Password Incorrect";
+
+            model.addAttribute("message", message);
+            return "pages/login";
+        }
+
+        session.setAttribute("user",user);
+
+        return "/dashboard";
+
+    }
+
 }
